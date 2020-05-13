ee28b01abede9440d3971be087c9c308726b00b1
Steven Topazio <sat@northpointprinting.com>
2020-05-13 16:19:36 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -25,7 +25,7 @@ def now_serving(katz_deli)
   if katz_deli.empty? 
     puts "There is nobody waiting to be served!"
   else 
-    puts "Currently serving: "
+    puts "Currently serving #{katz_deli.first}"
   
   
   
