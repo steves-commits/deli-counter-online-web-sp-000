f0a3793a822fbc83f967c0f4aff2f96e7894cea0
Steven Topazio <sat@northpointprinting.com>
2020-05-13 16:18:38 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -21,4 +21,8 @@ def take_a_number(katz_deli, name)
   puts "Welcome, #{name}. You are number #{katz_deli.size} in line."
 end
 
-def now_serving()
+def now_serving(katz_deli)
+  if katz_deli.empty? 
+  
+  
+  
